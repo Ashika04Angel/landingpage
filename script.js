bb0e6baf2a1c270959ef9b97d491fa5f2678a6ac
Ashika04Angel <ashika04angel@gmail.com>
2025-08-19 12:50:24 +0530

    Animation

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function handleSubmit(event){
         })
     }
 }
+const hero = document.getElementById('hero');
+const images = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg",
+    "images/pic3.jpg"
+];
+
+let index = 0 ;
+
+function changeBackground(){
+        hero.style.backgroundImage = `url('${images[index]}')`;
+         index = (index + 1) % images.length;
+}
+changeBackground();
+setInterval(changeBackground, 5000);
